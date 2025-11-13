c907ed15912611a4a8020e72c1002a65bf45b610
pingviini314159 <tandung270101@gmail.com>
2025-11-13 13:26:28 +0200

    testing on cross valuation

--- utils/side.py
+++ utils/side.py
@@ -5,6 +5,8 @@ import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
 from imblearn.over_sampling import ADASYN
+from sklearn.model_selection import cross_val_score, KFold
+from sklearn.svm import SVC
 from imblearn.pipeline import Pipeline
 from xgboost import XGBClassifier
 
@@ -17,17 +19,25 @@ X_train = db1.drop('Diabetes_binary', axis=1)
 X_test = db2.drop('Diabetes_binary', axis=1)
 y_test = db2['Diabetes_binary']
 
+svm_classifier = SVC(kernel = 'linear')
+kf = KFold(n_splits=10, shuffle= True, random_state=42)
+
+cross_val_res = cross_val_score(svm_classifier, X_train, y_train, cv=kf)
+
+print('CVR: ')
+for i, res in enumerate(cross_val_res,1):
+    print(f"Fold {i}: {res}%")
+    
+print("ACC:", cross_val_res.mean())
 
 pipeline_stage2 = Pipeline([
-    ('adasyn', ADASYN(random_state = 42)),
+    ('adasyn', ADASYN(n_neighbors=3,random_state = 42)),
     ('scaler', StandardScaler()),
     ('model', XGBClassifier(
         random_state=42,
         n_estimators=400,
-        learning_rate=0.05,
-        scale_pos_weight=1,
-        objective='binary:logistic'
-    ))
+        learning_rate=0.02,
+        scale_pos_weight=1))
 ])
 
 pipeline_stage2.fit(X_train, y_train)
@@ -38,24 +48,21 @@ db2_types_compare['Predicted_Type'] = y_pred_stage2
 db2_types_compare.to_csv('data/db2_types_compare.csv', index=False)
 print("Saved: data/db2_types_compare.csv")
 
-y_true = y_test
-y_pred = y_pred_stage2
 
 print("\nClassification Report:")
-print(classification_report(y_true, y_pred, zero_division=0))
+print(classification_report(y_test, y_pred_stage2, zero_division=0))
 
-cm = confusion_matrix(y_true, y_pred)
+cm = confusion_matrix(y_test, y_pred_stage2)
 print("\nConfusion Matrix:\n", cm)
 
-# Visualization
 sns.set(style='whitegrid', context='talk')
 
 
 plt.figure(figsize=(8, 4))
 bar_width = 0.35
 x = np.arange(2)
-actual_counts = pd.Series(y_true).value_counts().sort_index()
-pred_counts = pd.Series(y_pred).value_counts().sort_index()
+actual_counts = pd.Series(y_test).value_counts().sort_index()
+pred_counts = pd.Series(y_pred_stage2).value_counts().sort_index()
 
 plt.bar(x - bar_width/2, actual_counts, width=bar_width, label='Actual', color='skyblue')
 plt.bar(x + bar_width/2, pred_counts, width=bar_width, label='Predicted', color='salmon')

6b58e3f72868f43f757b932a9794b605c581bb9e
pingviini314159 <tandung270101@gmail.com>
2025-11-17 21:43:37 +0200

    add solution file

--- utils/side.py
+++ utils/side.py
@@ -4,11 +4,12 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import confusion_matrix, classification_report
-from imblearn.over_sampling import ADASYN
-from sklearn.model_selection import cross_val_score, KFold
-from sklearn.svm import SVC
-from imblearn.pipeline import Pipeline
+from sklearn.model_selection import StratifiedKFold, RandomizedSearchCV
 from xgboost import XGBClassifier
+from imblearn.pipeline import Pipeline
+from imblearn.over_sampling import ADASYN
+import joblib
+
 
 db1 = pd.read_csv('data/db1.csv')
 db2 = pd.read_csv('data/db2.csv')
@@ -19,44 +20,72 @@ X_train = db1.drop('Diabetes_binary', axis=1)
 X_test = db2.drop('Diabetes_binary', axis=1)
 y_test = db2['Diabetes_binary']
 
-svm_classifier = SVC(kernel = 'linear')
-kf = KFold(n_splits=10, shuffle= True, random_state=42)
 
-cross_val_res = cross_val_score(svm_classifier, X_train, y_train, cv=kf)
-
-print('CVR: ')
-for i, res in enumerate(cross_val_res,1):
-    print(f"Fold {i}: {res}%")
-    
-print("ACC:", cross_val_res.mean())
-
-pipeline_stage2 = Pipeline([
-    ('adasyn', ADASYN(n_neighbors=3,random_state = 42)),
+# Without ADASYN
+pipeline_tune = Pipeline([
     ('scaler', StandardScaler()),
     ('model', XGBClassifier(
-        random_state=42,
-        n_estimators=400,
-        learning_rate=0.02,
-        scale_pos_weight=1))
+        objective='binary:logistic',
+        eval_metric='logloss',
+        random_state=42
+    ))
 ])
 
-pipeline_stage2.fit(X_train, y_train)
-y_pred_stage2 = pipeline_stage2.predict(X_test)
+param_grid = {
+    'model__max_depth': [4, 6],
+    'model__learning_rate': [0.01, 0.05],
+    'model__n_estimators': [200, 400]
+}
+
+cv = StratifiedKFold(n_splits=3, shuffle=True, random_state=42)
+
+search = RandomizedSearchCV(
+    estimator=pipeline_tune,
+    param_distributions=param_grid,
+    n_iter=8,
+    cv=cv,
+    scoring='f1',
+    verbose=1,
+    n_jobs=1
+)
+
+search.fit(X_train, y_train)
+
+print("\nBest Params:", search.best_params_)
+print("Best CV Score:", search.best_score_)
+
+best_params = search.best_params_
+
+#With ADASYN
+pipeline = Pipeline([
+    ('adasyn', ADASYN(n_neighbors=3, random_state=42)),
+    ('scaler', StandardScaler()),
+    ('model', XGBClassifier(
+        objective='binary:logistic',
+        eval_metric='logloss',
+        random_state=42,
+        max_depth=best_params['model__max_depth'],
+        learning_rate=best_params['model__learning_rate'],
+        n_estimators=best_params['model__n_estimators']
+    ))
+])
+
+pipeline.fit(X_train, y_train)
+y_pred_stage2 = pipeline.predict(X_test)
 
 db2_types_compare = db2.copy()
 db2_types_compare['Predicted_Type'] = y_pred_stage2
-db2_types_compare.to_csv('data/db2_types_compare.csv', index=False)
-print("Saved: data/db2_types_compare.csv")
+
+# db2_types_compare.to_csv('data/db2_types_compare_final.csv', index=False)
 
 
-print("\nClassification Report:")
+print("\n=== CLASSIFICATION REPORT ===")
 print(classification_report(y_test, y_pred_stage2, zero_division=0))
 
 cm = confusion_matrix(y_test, y_pred_stage2)
 print("\nConfusion Matrix:\n", cm)
 
 sns.set(style='whitegrid', context='talk')
-
 
 plt.figure(figsize=(8, 4))
 bar_width = 0.35
@@ -74,7 +103,6 @@ plt.xticks(x, ['0', '1'])
 plt.legend()
 plt.tight_layout()
 plt.show()
-
 
 plt.figure(figsize=(6, 5))
 sns.heatmap(cm, annot=True, fmt='d', cmap='Greens')
